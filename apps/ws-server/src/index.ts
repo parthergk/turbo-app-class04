dd1423159282c598bf1c7753038efa701185a55e
parthergk <gauravkumar81464@gmail.com>
2025-04-12 13:31:55 +0530

    get data on frontend side

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,7 +1,13 @@
+import { prismaClient } from "@repo/db/client";
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port:8080});
 
-wss.on("connection", (ws)=>{
-
+wss.on("connection", async (ws)=>{
+    await prismaClient.user.create({
+        data:{
+            name : "Parther"
+        }
+    })
+    ws.send("connection is created successfully");
 })
