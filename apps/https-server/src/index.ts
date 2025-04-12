53a939866aeebf60b013f470b7588a6bdb3ef902
parthergk <gauravkumar81464@gmail.com>
2025-04-12 13:12:36 +0530

    first commit

---
+++ apps/https-server/src/index.ts
@@ -0,0 +1,18 @@
+import  Express, { Request, Response }  from "express";
+import {prismaClient} from "@repo/db/client";
+
+const app = Express();
+
+app.get('/', async(req:Request, res:Response)=>{
+    const newName = await prismaClient.user.create({
+        data:{
+            name: "Gaurav Kumar"
+        }
+    }) 
+    const name = newName.name;
+    res.json({message:"hello",name});
+})
+
+app.listen(3000, ()=>{
+    console.log("http server is runing on port 3000");
+})
